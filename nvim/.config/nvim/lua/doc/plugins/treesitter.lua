52194c7a68b0b3c8145f776dbedde7f8d43364fd
Jeff Smith <jeffrey.smith@dell.com>
2026-02-05 13:52:46 -0500

    Fix for treesitter

--- nvim/.config/nvim/lua/doc/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/doc/plugins/treesitter.lua
@@ -1,23 +1,19 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "main", -- Ensure you are tracking the new branch
+  branch = "main",
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
-    -- New version moved incremental selection to a separate community plugin
-    -- to keep the core lean. You can add it here if you want that feature back:
-    { "MeanderingProgrammer/treesitter-modules.nvim" },
+    "MeanderingProgrammer/treesitter-modules.nvim",
   },
   config = function()
-    -- 1. Import the NEW singular config module
+    -- USE 'config' (singular), NOT 'configs'
     local ts_config = require("nvim-treesitter.config")
 
-    -- 2. Configure basic features
     ts_config.setup({
       highlight = { enable = true },
       indent = { enable = true },
-      -- 'ensure_installed' is still supported in the setup table on main
       ensure_installed = {
         "json",
         "yaml",
@@ -31,20 +27,20 @@ return {
       },
     })
 
-    -- 3. Configure autotag (needs manual setup on the main branch)
-    require("nvim-ts-autotag").setup()
+    -- Restores the missing 'incremental_selection' logic
+    require("treesitter-modules").setup({
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
 
-    -- 4. NEW Incremental Selection (Native Neovim Way)
-    -- If you don't use the 'treesitter-modules' plugin above,
-    -- use these native mappings for similar behavior:
-    vim.keymap.set("n", "<C-space>", function()
-      require("nvim-treesitter.incremental_selection").init_selection()
-    end)
-    vim.keymap.set("x", "<C-space>", function()
-      require("nvim-treesitter.incremental_selection").node_incremental()
-    end)
-    vim.keymap.set("x", "<bs>", function()
-      require("nvim-treesitter.incremental_selection").node_decremental()
-    end)
+    -- Setup autotag separately
+    require("nvim-ts-autotag").setup()
   end,
 }
